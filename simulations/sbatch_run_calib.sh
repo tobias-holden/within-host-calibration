159d30683e68f88a9bf73e899b70b8b74ae3168e
tobiasmholden <tobiasmholden@gmail.com>
2024-10-15 22:48:36 -0500

    move suite/experiment/simulation files from calibration_testing to experiments folder. generalize paths in manifest and sbatch headers

--- simulations/sbatch_run_calib.sh
+++ simulations/sbatch_run_calib.sh
@@ -10,9 +10,16 @@
 #SBATCH --error=log/calib_IIVT-PT_VS_AGE_IIV.%j.err
 #SBATCH --output=log/calib_IIVT-PT_VS_AGE_IIV.%j.out
 
-
+# Purge modules
 module purge all
+
+# Activate virtual environment
 source activate /projects/b1139/environments/emod_torch_tobias
 
-cd /projects/b1139/calibration_scenarios/MII_variable_IIVT\-3/simulations
+# Navigate to project directory
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+cd simulations
+
+# Run calibrations
 python run_calib.py
