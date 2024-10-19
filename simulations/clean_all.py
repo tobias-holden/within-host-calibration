2ce46a2b6f1fd56cdc751b05e517e3a9138116d1
tobiasmholden <tobiasmholden@gmail.com>
2024-10-19 10:01:11 -0500

    add clean_logs() to erase files inside simulations/log

--- simulations/clean_all.py
+++ simulations/clean_all.py
@@ -3,9 +3,16 @@ import re
 import manifest
 
 def clean_analyzers():
+    print(manifest.CURRENT_DIR)
     for file in os.listdir(manifest.CURRENT_DIR):
         if re.match("run_analyzer_.*sh", file) or re.match("wait_analyzer_.*sh", file):
-            os.remove(file)
+            os.remove(os.path.join(manifest.CURRENT_DIR,file))
+
+def clean_logs():
+    log_dir= manifest.CURRENT_DIR / "log"
+    for file in os.listdir(log_dir):
+            os.remove(os.path.join(log_dir,file))
 
 if __name__ == "__main__":
     clean_analyzers()
+    clean_logs()
